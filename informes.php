a128dfec96ab4b76373dc879fa2d2d96f03ecb8f
Nicolas <nicolasgalvez1985@gmail.com>
2022-10-17 01:09:02 -0300

    Se agrega opción de mostrar max o min con info de las 3 tablas

--- informes.php
+++ informes.php
@@ -26,36 +26,60 @@ if (isset($_SESSION['usuario'])) {
 
 <body class="container">
     <div class="jumbotron text-center">
-        <h1>Informes</h1>
+        <h1>Informes sobre transacciones</h1>
+        <form action="informes.php" method="POST">
+            <input type="submit" name = "min" value="Mostrar mínima" class="btn btn-primary">
+            <input type="submit" name = "max" value="Mostrar máxima" class="btn btn-light">
+        </form>
     </div>
-        <div class= "d-flex justify-content-center">
+        <div class= "d-flex px-0 justify-content-center ">            
+            
             <div class="row">
-                <div class="col-sm-4">
-                    <div class="card">
-                        <div class="card-body">
-                            <h5 class="card-title">Transacción máxima</h5>
+                <div class="col-lg-4">
+                    <div class="card">                        
+                        <?php                            
+                            if (isset($_REQUEST['max'])){
+                                $muestra1 = '<div class="card-body " style="width: 33rem;>
+                                            <h2 class="card-title">Transacción máxima</h2>';        
+                            }else if (isset($_REQUEST['min'])){
+                                $muestra1 = '<div class="card-body " style="width: 33rem;>
+                                            <h2 class="card-title">Transacción mínima</h2>'; 
+                            }else{
+                                $muestra1 = '<div class="card-body " style="width: 33rem; display: none;">';
+                            }
+                            echo "$muestra1";
+                            ?>          
+                            <!-- <h5 class="card-title">Transacción máxima</h5> -->
                             <table class="table">
                                 <thead>
                                     <tr>
                                         <th scope="col">Cliente</th>
                                         <th scope="col">Transacción máxima</th>
                                         <th scope="col">Fecha</th>
+                                        <th scope="col">Empresa</th>
                                     </tr>
                                 </thead>
                                     <tbody>
                                         <?php
+
+                                        if (isset($_REQUEST['max'])){
+                                            $pedido = "MAX";
+                                        }else if (isset($_REQUEST['min'])){
+                                            $pedido = "MIN";
+                                        }
                                         $usuario = 'root';
-                                        $password = 'npEGa2014'; // cuidado, aca va el password db local de c/u
+                                        $password = 'password'; // cuidado, aca va el password db local de c/u
                                         $db = new PDO('mysql:host=localhost;dbname=Billetera', $usuario, $password);
-                                        $query = $db->prepare("SELECT MAX(montoTransaccion) AS PrecioMax, C.nombreCliente AS Nombre, fechaTransaccion AS fecha FROM billetera.transacciones T INNER JOIN billetera.clientes C WHERE T.clientes_idCliente = C.dniCliente ;
+                                        $query = $db->prepare("SELECT $pedido(montoTransaccion) AS PrecioMax, C.nombreCliente AS NombreC, E.nombreEmpresa AS NombreE, fechaTransaccion AS fecha FROM billetera.transacciones T INNER JOIN billetera.clientes C INNER JOIN billetera.empresas E WHERE T.clientes_idCliente = C.dniCliente AND E.cuitEmpresa = T.empresas_idEmpresa ;
                                         ");
                                         $query->execute();
                                         $data = $query->fetchAll();
                                         foreach ($data as $valores) :
                                             echo '<tr>';
-                                            echo '<th scope="row">' . $valores["Nombre"] . '</th>';
+                                            echo '<th scope="row">' . $valores["NombreC"] . '</th>';
                                             echo '<th scope="row">' . $valores["PrecioMax"] . '</th>';
                                             echo '<th scope="row">' . $valores["fecha"] . '</th>';
+                                            echo '<th scope="row">' . $valores["NombreE"] . '</th>';
                                             echo '</tr>';
                                         endforeach;
                                         ?>
@@ -63,9 +87,9 @@ if (isset($_SESSION['usuario'])) {
                             </table>
                         </div>
                     </div>
+                </div>
             </div>
         </div>
-    </div>
     
 </body>
 
